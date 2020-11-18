bf3af68004ee4e250cf061881206df3ace4ff787
uri heap <u.heep@tutanota.com>
2020-11-17 21:47:05 -0600

    unit_tests/src/boostTestGeometry.cpp: added test case 2; TODO: more test cases

--- unit_tests/src/boostTestGeometry.cpp
+++ unit_tests/src/boostTestGeometry.cpp
@@ -37,4 +37,37 @@ BOOST_AUTO_TEST_CASE( boostTestGeometry1 )
   BOOST_CHECK_EQUAL( nullptr, aMultiplicity );
 }
 
+// test with a corrupt CIF file:
+BOOST_AUTO_TEST_CASE( boostTestGeometry2 )
+{
+  cbc::Geometry  geom;
+  geom.readCIF( "corrupt_1.cif" );
+//  geom.readCIF( "hydrogen.cif" );
+
+  const unsigned&  numAtoms  =  geom.getNumAtoms();
+  const unsigned&  numChains  =  geom.getNumChains();
+  const unsigned&  numResidues  =  geom.getNumResidues();
+  BOOST_CHECK_EQUAL( static_cast<unsigned>( 0 ), numAtoms );
+  BOOST_CHECK_EQUAL( static_cast<unsigned>( 0 ), numChains );
+  BOOST_CHECK_EQUAL( static_cast<unsigned>( 0 ), numResidues );
+
+  const unsigned short  numAtomsInResidue  =  geom.getNumAtomsInResidue( 0 );
+  const unsigned        numResiduesInChain  =  geom.getNumResiduesInChain( 0 );
+  const char            residueName  =  geom.getResidueName( 0 );
+
+  BOOST_CHECK_EQUAL( static_cast<unsigned short>( 0 ), numAtomsInResidue );
+  BOOST_CHECK_EQUAL( static_cast<unsigned>( 0 ), numResiduesInChain );
+  BOOST_CHECK_EQUAL( static_cast<char>( '_' ), residueName );
+
+  const unsigned short *  aPeriodicNumbers  =  geom.getPeriodicNumbers();
+  double *  aCoordinates  =  geom.getCoordinates();
+  double *  aCharges      =  geom.getCharges();
+  const unsigned short *  aMultiplicity  =  geom.getMultiplicity();
+
+  BOOST_CHECK_EQUAL( nullptr, aPeriodicNumbers );
+  BOOST_CHECK_EQUAL( nullptr, aCoordinates );
+  BOOST_CHECK_EQUAL( nullptr, aCharges );
+  BOOST_CHECK_EQUAL( nullptr, aMultiplicity );
+}
+
 BOOST_AUTO_TEST_SUITE_END()
