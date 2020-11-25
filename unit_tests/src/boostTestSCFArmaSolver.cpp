cd764f7ab85c60fa234748b9c4fa5a39d89e1c66
uri heap <u.heep@tutanota.com>
2020-11-24 19:25:06 -0600

    unit_tests/src/boostTestSCFArmaSolver.cpp: init commit

---
+++ unit_tests/src/boostTestSCFArmaSolver.cpp
@@ -0,0 +1,19 @@
+// test suite: testSCFArmaSolver
+//
+
+#include <boost/test/unit_test.hpp>
+
+#include "scf_arma_solver.hpp"
+
+
+BOOST_AUTO_TEST_SUITE( testSCFArmaSolver )
+
+BOOST_AUTO_TEST_CASE( testSCFArmaSolver1 )
+{/*
+  cbc::SCFArmaSolver  solver( geometry,
+                              basisSet
+                            );
+  */
+}
+
+BOOST_AUTO_TEST_SUITE_END()
